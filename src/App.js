e96dd85341dd3aa5f863860bc43d9a62a6730f8b
Jesse Navarro <user42@Jesses-MBP.home>
2022-01-08 13:39:27 -1000

    setup tailwindcss

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// components
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App bg-green flex justify-center items-center">
+      <div className="">
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+
+function Home() {
+    return (
+        <div className="Home">
+            <h1>Shape Shifters</h1>
+            <h6>Start</h6>
+        </div>
+    )
+}
+
+export default Home
